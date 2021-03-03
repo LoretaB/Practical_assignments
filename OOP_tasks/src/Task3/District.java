2b138f3d863e734fc4f438bd32d370d21efcc5db
LoretaB <loreta.bolsevica@inbox.lv>
2021-03-03 23:56:45 +0200

    OOP Task 3 added

---
+++ OOP_tasks/src/Task3/District.java
@@ -0,0 +1,81 @@
+package Task3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class District {
+    private String title;
+    private String city;
+    private int districtID;
+    private Officer[] officersInTheDistrict;
+
+    public District() {
+    }
+
+    public District(String title, String city, int districtID, Officer[] officersInTheDistrict) {
+        this.title = title;
+        this.city = city;
+        this.districtID = districtID;
+        this.officersInTheDistrict = officersInTheDistrict;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getDistrictID() {
+        return districtID;
+    }
+
+    public void setDistrictID(int districtID) {
+        this.districtID = districtID;
+    }
+
+    public Officer[] getOfficersInTheDistrict() {
+        return officersInTheDistrict;
+    }
+
+    public void setOfficersInTheDistrict(Officer[] officersInTheDistrict) {
+        this.officersInTheDistrict = officersInTheDistrict;
+    }
+
+    public String toString() {
+        return "Title: " + getTitle() + "\nCity: " + getCity() + "\nDistrict ID: " + getDistrictID() + "\nOfficers in the district: \n" + Arrays.toString(getOfficersInTheDistrict()) + "\n";
+    }
+
+    public boolean addNewOfficer(Officer officer) {
+        ArrayList<Officer> sourceList = new ArrayList<>(Arrays.asList(getOfficersInTheDistrict()));
+        sourceList.add(officer);
+        setOfficersInTheDistrict(sourceList.toArray(new Officer[0]));
+        return true;
+    }
+
+    public boolean removeOfficer(Officer officer) {
+        ArrayList<Officer> sourceList = new ArrayList<>(Arrays.asList(getOfficersInTheDistrict()));
+        sourceList.remove(officer);
+        setOfficersInTheDistrict(sourceList.toArray(new Officer[0]));
+        return true;
+    }
+
+    public float calculateAvgLevelInDistrict() {
+        Officer[] officersArray = getOfficersInTheDistrict();
+        float sum = 0;
+        for (Officer officer : officersArray) {
+            sum += officer.calculatedLevel(officer.getCrimesSolved());
+        }
+        float averageLevel = sum / officersArray.length;
+        return averageLevel;
+    }
+}
---
+++ OOP_tasks/src/Task3/Officer.java
@@ -0,0 +1,49 @@
+package Task3;
+
+public class Officer extends Person {
+    private int officerID;
+    private int crimesSolved;
+
+    public Officer() {
+    }
+
+    public Officer(String name, String surname, int officerID, int crimesSolved) {
+        super(name, surname);
+        this.officerID = officerID;
+        this.crimesSolved = crimesSolved;
+    }
+
+    public int getOfficerID() {
+        return officerID;
+    }
+
+    public void setOfficerID(int officerID) {
+        this.officerID = officerID;
+    }
+
+    public int getCrimesSolved() {
+        return crimesSolved;
+    }
+
+    public void setCrimesSolved(int crimesSolved) {
+        this.crimesSolved = crimesSolved;
+    }
+
+    public String toString() {
+        return "Name: " + getName() + " Surname: " + getSurname() + " Officer ID: " + getOfficerID() + " Crimes solved: " + getCrimesSolved() + "\n";
+    }
+
+    public int calculatedLevel(int crimesSolved) {
+        int officeLevel = 0;
+        if (crimesSolved < 20) {
+            officeLevel = 1;
+        } else if (crimesSolved >= 20 && crimesSolved < 40) {
+            officeLevel = 2;
+        } else if (crimesSolved >= 40) {
+            officeLevel = 3;
+        }
+        return officeLevel;
+    }
+
+}
+
